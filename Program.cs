d0f39a7790c1259b817fced90296eb90925bd924
Andres Sainz <andresmsainz@gmail.com>
2021-05-16 14:29:57 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Snippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snippets!");
+
+            //for + <t> + <t>
+            //for (int i = 0; i < length; i++)
+            //{
+
+            // }
+
+            //foreach + <t> + <t>
+            //foreach (var item in collection)
+            //{
+
+            //}
+
+            //cw + <t> + <t>
+            Console.WriteLine( );
+
+            //switch + <t> + <t>
+            //switch (switch_on)
+            //{
+            //    default:
+            //}
+
+            //do + <t> + <t>
+            do
+            {
+
+            } while (true);
+
+            //while + <t> + <t>
+            while (true)
+            {
+
+            }
+
+            //if + <t> + <t>
+            if (true)
+            {
+
+            }
+        }
+
+        //ctor  + <t> + <t>
+        public Program()
+        {
+
+        }
+
+        //prop + <t> + <t>
+        public int MyProperty { get; set; }
+
+        //propfull  + <t> + <t>
+        private int myVar;
+
+        public int MyProperty1
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+        //enum
+        enum MyEnum
+        {
+
+        }
+
+    }
+}
